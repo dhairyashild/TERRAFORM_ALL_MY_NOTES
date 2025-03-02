f8e2fe7ba31dc84c808e6266ce4273fc49b4db7d
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2025-03-02 11:39:06 +0530

    Update 1-PROVIDER.tf

--- 1-PROVIDER.tf
+++ 1-PROVIDER.tf
@@ -21,7 +21,8 @@ provider "aws" {
 
 #required_providers: Specifies the provider and version.  
 #provider "aws": Configures the provider's settings, such as the region.
-
+#3 it wont allow to upgrade/dwngrade provider version —so if u want tpo upgrade then delete this file then update
+#4 if u upgrade version then CHECK code in .tf files also for changes, there are certain code changes as per each new provider upgrade
 
 #video link
 https://www.youtube.com/watch?v=Jj0M6waGJVw
