fdaf2103af8a832cf8da786568e2241e13e360d6
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2025-03-17 12:40:39 +0530

    Create ec2_data.tf

---
+++ DATASOURCE/ec2_data.tf
@@ -0,0 +1,12 @@
+data "aws_instance" "myawsinstance" {
+  filter {
+    name   = "tag:Name"
+    values = ["Terraform EC2"]
+  }
+
+  depends_on = [aws_instance.ec2_example] 
+}
+
+
+output "fetched_info_from_aws" { 
+  value = data.aws_instance.myawsinstance.public_ip 
